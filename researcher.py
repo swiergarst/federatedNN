a908c6b75d15c8bcb0614c3901d04d534131f815
swier <swier.garst@gmail.com>
2022-02-15 10:37:02 +0100

    final thesis version

--- researcher.py
+++ researcher.py
@@ -43,39 +43,46 @@ client.setup_encryption(privkey)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = 'SGD'
-lr_local = 5e-3
+lr_local = 5e-2
 lr_global = 1
 local_epochs = 1
 
-local_batch_amt = 1
+local_batch_amt = 10    
 
 ids = [org['id'] for org in client.collaboration.get(1)['organizations']]
 
 #dataset and booleans
-dataset = '3node' # options: MNIST_2class, MNIST_4class, MNIST, fashion_MNIST, A2
-week = "datafiles/w23/"
+dataset = 'MNIST_2class' # options: MNIST_2class, MNIST_4class, MNIST, fashion_MNIST, A2
+week = "datafiles/w27/"
 
-model_choice = "FNN"
-save_file = False
+model_choice = "CNN"
+save_file = True
 class_imbalance = False
-sample_imbalance = False
+sample_imbalance = True
 use_scaffold= False
 use_c = True
 use_sizes = True
 prefix = get_save_str(dataset, model_choice, class_imbalance, sample_imbalance, use_scaffold, use_sizes, lr_local, local_epochs, local_batch_amt)
 
 #federated settings
-num_global_rounds = 20
+num_global_rounds = 100
 num_clients = 10
-num_runs = 1
-seed_offset = 0
+num_runs = 3
+seed_offset = 1
 
 
 prevmap = heatmap(num_clients, num_global_rounds)
 newmap = heatmap(num_clients, num_global_rounds)
 #if use_scaffold:
 cmap = heatmap(num_clients , num_global_rounds)
+c_log = np.zeros((num_global_rounds))
+ci_log = np.zeros((num_global_rounds, num_clients))
 
+#quick hack b/c i was too lazy to update the model part of the image
+if dataset == "2node":
+    dataset_tosend = "3node"
+else :
+    dataset_tosend = dataset
 
 ### main loop
 for run in range(num_runs):
@@ -88,9 +95,10 @@ for run in range(num_runs):
     datasets, parameters, X_test, y_test, c, ci = get_config(dataset, model_choice, num_clients, class_imbalance, sample_imbalance)
     ci = np.array(ci)
     old_ci = np.array([c.copy()] * num_clients)
+    
 
     #test model for global testing
-    testModel = model(dataset, model_choice, c)
+    testModel = model(dataset_tosend, model_choice, c)
     testModel.double()
     for round in range(num_global_rounds):
         for i in range(num_clients):
@@ -102,7 +110,7 @@ for run in range(num_runs):
         task_list = np.empty(num_clients, dtype=object)
         
 
-        for i, org_id in enumerate(ids):
+        for i, org_id in enumerate(ids[0:num_clients]):
             #print("org id \t ids[i]")
             #print(org_id, "\t", ids[i])
 
@@ -120,12 +128,12 @@ for run in range(num_runs):
                         'scaffold' : use_scaffold,
                         'c' : c, 
                         'ci': ci[i],
-                        'dataset' : dataset, 
+                        'dataset' : dataset_tosend, 
                         'use_c' : use_c
                         }
                 },
                 name =  prefix + ", round " + str(round),
-                image = "sgarst/federated-learning:fedNN9",
+                image = "sgarst/federated-learning:fedNN10",
                 organization_ids=[org_id],
                 collaboration_id= 1
                 
@@ -164,6 +172,9 @@ for run in range(num_runs):
             parameters, c = scaffold(dataset, model_choice, parameters, local_parameters, c, old_ci, ci, lr_global, use_c = use_c)
             #print("old ci: ", old_ci)
             cmap.save_round(round, ci, c)
+            c_log[round] = c['lin_layers.0.weight'].max()
+            for i in range(num_clients):
+                ci_log[round, i] = ci[i]['lin_layers.0.weight'].max()
         else:
             parameters = average(local_parameters, dataset_sizes, None, dataset, model_choice, use_imbalances=False, use_sizes= use_sizes)
 
@@ -172,8 +183,8 @@ for run in range(num_runs):
         # 'global' test
         testModel.set_params(parameters)
         complete_test_results[round]  = testModel.test(X_test, y_test, criterion)
-        #if (round % 10) == 0:
-        #    clear_database()
+        if (round % 10) == 0:
+            clear_database()
     
     if save_file:
         if use_scaffold:    
@@ -200,7 +211,15 @@ x = np.arange(num_global_rounds)
 #newmap.show_map()
 
 #plt.plot(x, np.mean(acc_results, axis=1, keepdims=False)[0,:])
+for i in range (num_clients):#
+   plt.plot(x, acc_results[i,:])
+legend = ["client " + str(i) for i in range(num_clients)]
+legend.append("full")
 plt.plot(x, complete_test_results)
+plt.legend(legend)
 plt.show()
 
+plt.plot(x, c_log)
+plt.plot(x, ci_log)
+#plt.show()
     ### generate new model parameters
